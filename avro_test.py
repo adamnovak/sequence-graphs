1145dcab6132f03f036906fe5b541420a1bb3443
Adam Novak <anovak@soe.ucsc.edu>
2013-12-02 10:05:29 -0800

    Showing circular object graphs don't work.

--- avro_test.py
+++ avro_test.py
@@ -5,8 +5,15 @@ from avro.io import DatumReader, DatumWriter
 schema = avro.schema.parse(open("user.avsc").read())
 
 writer = DataFileWriter(open("users.avro", "w"), DatumWriter(), schema)
-writer.append({"name": "Alyssa", "favorite_number": 256})
-writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
+
+alyssa = {"name": "Alyssa", "favorite_number": 256}
+ben = {"name": "Ben", "favorite_number": 7, "favorite_color": "red"}
+
+alyssa["favorite_user"] = ben
+ben["favorite_user"] = alyssa
+
+writer.append(alyssa)
+writer.append(ben)
 writer.close()
 
 reader = DataFileReader(open("users.avro", "r"), DatumReader())
