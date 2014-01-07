6e3d3c225d8cc2842073c44d400014011bf6cb20
Adam Novak <anovak@soe.ucsc.edu>
2014-01-07 15:29:55 -0800

    Can now read a file.

--- importVCF/src/main/scala/importVCF.scala
+++ importVCF/src/main/scala/importVCF.scala
@@ -5,6 +5,8 @@ import org.apache.spark.graph._
 import edu.ucsc.soe.sequencegraph
 import edu.ucsc.soe.sequencegraph._
 
+import java.io.File
+
 //import org.broadinstitute.variant.vcf.VCFFileReader;
 
 import org.rogach.scallop._;
@@ -12,23 +14,31 @@ import org.rogach.scallop._;
 object SequenceGraphs {
     def main(args: Array[String]) {
         
-        // Option parsing code more or less stolen from various Scallop examples.
+        // Option parsing code more or less stolen from various Scallop
+        // examples.
         val opts = new ScallopConf(args) {
+            guessOptionName = true
             banner("""Usage: importVCF [OPTION]... vcfFile
                 |Import a VCF file to sequence graph format.
                 |Options:
                 |""".stripMargin)
-        
-            val vcfFilename = trailArg[String]("vcfFilename", required = true)
             
-            val version = opt[Boolean]("version", noshort = true, 
+            val vcfFile = trailArg[File](required = true)
+            
+            val version = opt[Boolean](noshort = true, 
                 descr = "Print version")
-            val help = opt[Boolean]("help", noshort = true, 
+            val help = opt[Boolean](noshort = true, 
                 descr = "Show this message")
 
         } 
         
-        println(opts.vcfFilename)
+        // Get the actual File or die trying, and then make a Source from it.
+        val vcfFile = scala.io.Source.fromFile(opts.vcfFile.get.get)
+        
+        println(vcfFile)
+        val lines = vcfFile.getLines.toList
+        
+        lines.map(println)
         
         /*    
         val vcfFile = File(args
