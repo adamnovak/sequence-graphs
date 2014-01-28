03a53295c36c57ab16d384fa82da1bf1e05421ef
Adam Novak <anovak@soe.ucsc.edu>
2014-01-28 13:00:50 -0800

    Making simpleapp portable properly

--- simpleapp/src/main/scala/SimpleApp.scala
+++ simpleapp/src/main/scala/SimpleApp.scala
@@ -5,11 +5,9 @@ import org.apache.spark.SparkContext._
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/pod/home/anovak/build/graphx/README.md" // Should be some file on your system
-    val sc = new SparkContext("mesos://localhost:5050", "Simple App", "/pod/home/anovak/build/incubator-spark-old",
+    val logFile = "simple.sbt" // Should be some file on your system
+    val sc = new SparkContext("mesos://localhost:5050", "Simple App", System.getenv("SPARK_HOME"),
       List("target/scala-2.9.3/simple-project_2.9.3-1.0.jar"))
-      
-    //val graphLoaded = GraphLoader.textFile(sc, "followers.tsv", (x: Array[String]) => 5)
       
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
