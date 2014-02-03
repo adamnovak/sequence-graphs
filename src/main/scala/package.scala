cc9e7e3bd0d9cd1ab855107d214848916f04a54b
Adam Novak <anovak@soe.ucsc.edu>
2014-02-03 15:32:35 -0800

    Adding SequenceGraphPen and some useful implicits to make constructing Sequence Graphs significantly easier, especially in parallel.

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -6,12 +6,17 @@ package edu.ucsc
  * Package object that contains implicits needed throughout the entire package,
  * and/or needed to use the package properly.
  *
- * Mostly this is orderings for Avro records. Avro defines a way to order
+ * Mostly this is:
+ *
+ * Orderings for Avro records. Avro defines a way to order
  * records, and indeed Avro-generated Java code all inherits from a base class,
  * comparable with itself, that actually can sort and compare things. However,
  * in Scala, this results in all Avro objects being comparable against the base
  * record type but not its subtypes; this results in the implicit production of
  * an Ordering for the base type but no Orderings for the subtypes.
+ *
+ * Implicit constructors for Avro records. We don't get to add them ourselves to
+ * the classes, so we make do here and provide some syntactic sugar.
  *
  * TODO: We should move to Scala 2.10+ and use macros to generate all of the
  * Avro orderings.
@@ -33,6 +38,31 @@ package object genome {
     implicit val sideOrdering = Ordering.by { (side: Side) =>
         side.position
     }
+    
+    /**
+     * Allow a Side to be used in place of its Long ID. We do this as a down-
+     * conversion (rather than converting Longs up to Sides) because otherwise
+     * we'd have to make up info for the other Side fields (or at least null
+     * them out), and could get into trouble. API users will just have to figure
+     * out that they can use Sides wherever a Long is called for.
+     */
+    implicit def Side2Long(side: Side) = side.id
+    
+    /**
+     * Auto-Magically make PloidyBounds for fixed ploidies.
+     */
+    implicit def Int2PloidyBounds(ploidy: Int) = {
+        new PloidyBounds(ploidy, null, null)
+    }
+    
+    /**
+     * Auto-Magically make PloidyBounds for 2-tuple ranges.
+     */
+    implicit def IntTuple2PloidyBounds(bounds: (Int, Int)) = {
+        new PloidyBounds(bounds._1, bounds._2, null)
+    }
+    
+    
     
 }
 
