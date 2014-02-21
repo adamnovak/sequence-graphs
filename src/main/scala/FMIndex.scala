7835ebf11fdc291f90837e32dd2983f620f31f3f
Adam Novak <anovak@soe.ucsc.edu>
2014-02-21 15:46:42 -0800

    Switching over to upstream contexts and sending out Positions, and pulling out the brute-force N^2 mapping stuff into its own trait

--- src/main/scala/FMIndex.scala
+++ src/main/scala/FMIndex.scala
@@ -1,5 +1,10 @@
 package edu.ucsc.genome
 
+// We use edu.ucsc.genome.Position objects to represent our positions. Contig is
+// the actual haplotype name, base is the index from the start of the forward
+// strand, and face is the strand (LEFT for forward strand, RIGHT for reverse
+// strand, since everything maps by upstream context).
+
 /**
  * A generic interface for implementations of an FMIndex and its associated
  * operations.
@@ -16,7 +21,7 @@ trait FMIndex {
      * Return the positions at which occurrences of the given pattern
      * start.
      */
-    def locate(pattern: String): Iterator[Long]
+    def locate(pattern: String): Iterator[Position]
     
 }
 
@@ -28,46 +33,36 @@ trait FancyFMIndex extends FMIndex {
     /**
      * Return the minimum number of characters from the iterator required to get
      * exactly one unique match, or None if no unique match exists. Interprets
-     * the iterator as specifying characters in string order (so characters will
-     * be pulled off and appended to the end of the search pattern).
+     * the iterator as specifying characters in reverse string order (so
+     * characters will be pulled off and prepended to the start of the search
+     * pattern).
      */
     def minUnique(characters: Iterator[Char]): Option[Long]
+    
+    /**
+     * Return the position to which the given base in the given string uniquely
+     * maps, or None if it does not uniquely map. Maps by upstream context (so
+     * base 0, for example, probably won't map).
+     */
+    def map(context: String, base: Int): Option[Position]
 
 }
 
 /**
- * An FMIndex that calls out to the "rlcsa_grep" CLI command to perform its
- * operations. Quite slow but requires only that rlcsa_grep be on the user's
- * PATH in order to work.
+ * An FMIndex that implements its fancier mapping features based on repeated
+ * count operations rather than more efficient customized backwards search.
  *
- * Takes an RLCSA basename, which was the original name of the indexed file
- * (which may no longer really exist).
+ * Mapping on such an index will be O(n^2) instead of O(n) as it should be.
  */
-class RLCSAGrepFMIndex(basename: String) extends FancyFMIndex {
-    import scala.sys.process._
+trait BruteForceFMIndex extends FancyFMIndex {
 
-    def count(pattern: String): Long = {
-        // Just run the command to count occurrences and intify its only output
-        // line.
-        Seq("rlcsa_grep", "-t", pattern, basename).!!.replace("\n", "").toLong
-    }
-    
-    def locate(pattern: String): Iterator[Long] = {
-        // Run the command to get all the occurrence positions (in global
-        // concatenated coordinates), and asynchronously map its resulting lines
-        // to longs.
-        Seq("rlcsa_grep", "-s", pattern, basename).lines.map {
-            _.replace("\n", "").toLong
-        }.iterator
-    }
-    
     def minUnique(characters: Iterator[Char]): Option[Long] = {
         var pattern = ""
         
         while(count(pattern) > 1 && characters.hasNext) {
             // We haven't found a unique match, and we have more context. Use
             // it.
-            pattern += characters.next
+            pattern = characters.next + pattern
         }
         
         if(count(pattern) == 1) {
@@ -80,4 +75,54 @@ class RLCSAGrepFMIndex(basename: String) extends FancyFMIndex {
         
     }
     
+    def map(context: String, base: Int): Option[Position] = {
+        // Map based on upstream context
+        
+        // Grab what's upstream. TODO: will this copy a whole chromosome?
+        val upstreamContext = context.substring(0, base + 1).reverse.iterator
+        
+        minUnique(upstreamContext) match {
+            case Some(neededCharacters) => 
+                // We can map with a certain amoutn of context. Grab that
+                // context and map.
+                
+                // TODO: Roll all these operations together at the C level so we
+                // can just backwards search until we have a unique match or
+                // fail.
+                // TODO: Make this work with longs.
+                Some(locate(context.substring(base - neededCharacters.toInt + 1, 
+                    base + 1)).next)
+            case None =>
+                // We either ran out of context or found nothing matching.
+                None
+        }
+    }
+    
+}
+
+/**
+ * An FMIndex that calls out to the "rlcsa_grep" CLI command to perform its
+ * operations. Quite slow but requires only that rlcsa_grep be on the user's
+ * PATH in order to work.
+ *
+ * Takes an RLCSA basename, which was the original name of the indexed file
+ * (which may no longer really exist).
+ */
+class RLCSAGrepFMIndex(basename: String) extends BruteForceFMIndex {
+    import scala.sys.process._
+
+    def count(pattern: String): Long = {
+        // Just run the command to count occurrences and intify its only output
+        // line.
+        Seq("rlcsa_grep", "-t", pattern, basename).!!.replace("\n", "").toLong
+    }
+    
+    def locate(pattern: String): Iterator[Position] = {
+        // Run the command to get all the occurrence positions (in global
+        // concatenated coordinates), and asynchronously map its resulting lines
+        // to longs.
+        Seq("rlcsa_grep", "-s", pattern, basename).lines.map { (line) =>
+            new Position("index", line.replace("\n", "").toLong, Face.LEFT)
+        }.iterator
+    }
 }
