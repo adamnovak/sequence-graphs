b73ea9cc2f44d16f5e416561813bd5502a0aede2
Adam Novak <anovak@soe.ucsc.edu>
2014-04-17 10:17:26 -0700

    Adding a partial port of FMDIndex

---
+++ createIndex/FMDIndex.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "FMDIndex.hpp"
+
+FMDIndex::FMDIndex(std::string basename): fmd(basename), names(), lengths() {
+    // We already loaded the index itself in the initializer. Go load the
+    // length/order metadata.
+    
+    // Open the contig name/length file for reading.
+    std::ifstream contigFile((basename + ".chrom.sizes").c_str());
+    
+    // Have a string to hold each line in turn.
+    std::string line;
+    while(std::getline(contigFile, line)) {
+        // For each <contig>\t<length> pair...
+        
+        // Find the \t
+        size_t separator = line.find('\t');
+        
+        // Split into name and length
+        std::string contigName = line.substr(0, separator - 1);
+        std::string contigLength = line.substr(separator + 1, line.size() - 1);
+        
+        // Parse the length
+        long long int lengthNumber = atoll(contigLength.c_str());
+        
+        // Add it to the vector of names in number order.
+        names.push_back(contigName);
+        
+        // And the vector of sizes in number order
+        lengths.push_back(lengthNumber);
+    }
+    // Close up the contig file. We read our contig metadata.
+    contigFile.close();
+    
+}
+
+CSA::usint FMDIndex::getContigNumber(CSA::pair_type base) const {
+    // What contig corresponds to that text? Contigs all have both strands.
+    return base.first / 2;
+}
+
+CSA::usint FMDIndex::getStrand(CSA::pair_type base) const {
+    // What strand corresponds to that text? Strands are forward, then reverse.
+    return base.first % 2;
+}
+
+CSA::usint FMDIndex::getOffset(CSA::pair_type base) const {
+    // What base offset, 1-based, from the left, corresponds to this pair_type,
+    // which may be on either strand.
+    if(getStrand(base) == 0) {
+        // We're on the forward strand; just correct for the 1-basedness offset.
+        return base.second + 1;
+    } else {
+        // We're on the reverse strand, so we measured from the end.
+        return lengths[getContigNumber(base)] - base.second;
+    }
+}
---
+++ createIndex/FMDIndex.hpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <rlcsa/fmd.h>
+
+
+/**
+ * A class that encapsulates access to an FMDIndex, consisting of the underlying
+ * FMD and some auxilliary structures (cointig names and lengths) that we store
+ * along with it.
+ */
+class FMDIndex {
+public:
+    /**
+     * Load an FMD and metadata from the given basename.
+     */
+    FMDIndex(std::string basename);
+    
+    /**
+     * Get the contig number (not the text number) from a (text, offset) pair.
+     */
+    CSA::usint getContigNumber(CSA::pair_type base) const;
+    
+    /**
+     * Get the strand from a (text, offset) pair: either 0 or 1.
+     */
+    CSA::usint getStrand(CSA::pair_type base) const;
+    
+    /**
+     * Given a pair_type representing a base, and a vector of contig lengths by
+     * number, determine the specified base's offset from the left of its contig,
+     * 1-based.
+     */
+    CSA::usint getOffset(CSA::pair_type base) const;
+    
+    /**
+     * Holds the actual underlying FMD.
+     */
+    CSA::FMD fmd;
+    
+    /**
+     * Holds the names of all the contigs.
+     */
+    std::vector<std::string> names;
+    
+    /**
+     * Holds the lengths of all the contigs, in the same order.
+     */
+    std::vector<long long int> lengths;
+
+};
