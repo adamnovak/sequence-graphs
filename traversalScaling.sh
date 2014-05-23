922666f8825fe2c01e45c1e44f6e916a7952b05b
Adam Novak <anovak@soe.ucsc.edu>
2014-05-23 14:41:14 -0700

    Making parallel harness script work

--- traversalScaling.sh
+++ traversalScaling.sh
@@ -8,22 +8,48 @@ set -e
 # Grab the FASTA to index.
 FASTA="${1}"
 
-# Grab a temp directory
-INDEX=`mktemp -d`
+# Make a directory to aggregate output from background subshells.
+OUT_DIR=`mktemp -d`
 
 for CONTEXT in {1..30}
 do
-    # Run on the FASTA file for each context.
-    echo "#Running for ${CONTEXT} bases"
-    # Grab the line for the second traversal and extract the first number: wall clock time in seconds.
-    TIME_TAKEN=`./createIndex.sh ${INDEX} ${FASTA} --context ${CONTEXT} 2>&1 | grep "[timer - Level Index Construction]" | sed 's/[^0-9]*\([0-9]\+\.[0-9]\+\).*/\1/'`
+    (
+        # Grab a temp directory
+        INDEX=`mktemp -d`
+        
+        # Pick a temp file to send our output to.
+        OUT_FILE="${OUT_DIR}/${CONTEXT}.tsv"
+        
+        # Make a scratch file
+        SCRATCH_FILE=`mktemp`
     
-    # Print them out as a two-column TSV
-    printf "${CONTEXT}\t${TIME_TAKEN}\n"
-
+        # Run on the FASTA file for each context.
+        createIndex/createIndex ${INDEX} ${FASTA} --context ${CONTEXT} &> ${SCRATCH_FILE}
+        
+        # Grab the line for the second traversal and extract the first number: wall clock time in seconds.
+        TIME_TAKEN=`cat ${SCRATCH_FILE} | grep "timer - Level Index Construction" | sed 's/[^0-9]*\([0-9]\+\.[0-9]\+\).*/\1/'`
+        
+        # Print them out as a two-column TSV
+        printf "${CONTEXT}\t${TIME_TAKEN}\n" >> "$OUT_FILE"
+        
+        echo "# Context ${CONTEXT} done in ${TIME_TAKEN}"
+        
+        # Get rid of the temp directory.
+        rm -Rf ${INDEX}
+        
+        # And the scratch file
+        rm ${SCRATCH_FILE}
+    ) &
+    
 done
 
-# Get rid of the temp directory.
-rm -Rf ${INDEX}
+# Wait for all the subshells to finish.
+wait
+
+# Aggregate all the output
+cat ${OUT_DIR}/*.tsv
+
+# Remove aggregated results.
+rm -Rf ${OUT_DIR}
 
 
