c26b6c166b521012dd5e3211b2918d279561e84a
Adam Novak <anovak@soe.ucsc.edu>
2014-05-23 15:11:14 -0700

    Making sure to batch parallel jobs in an effort not to crash into ulimit -u on total number of threads

--- traversalScaling.sh
+++ traversalScaling.sh
@@ -11,6 +11,9 @@ FASTA="${1}"
 # Make a directory to aggregate output from background subshells.
 OUT_DIR=`mktemp -d`
 
+# Don't go crazy on the parallel processing; each needs 100 threads.
+PARALLEL_LIMIT=5
+
 for CONTEXT in {1..30}
 do
     (
@@ -24,7 +27,7 @@ do
         SCRATCH_FILE=`mktemp`
     
         # Run on the FASTA file for each context.
-        createIndex/createIndex ${INDEX} ${FASTA} --context ${CONTEXT} &> ${SCRATCH_FILE}
+        createIndex/createIndex ${INDEX} ${FASTA} --context ${CONTEXT} &> ${SCRATCH_FILE} || echo "# Context ${CONTEXT} failed!" && exit 
         
         # Grab the line for the second traversal and extract the first number: wall clock time in seconds.
         TIME_TAKEN=`cat ${SCRATCH_FILE} | grep "timer - Level Index Construction" | sed 's/[^0-9]*\([0-9]\+\.[0-9]\+\).*/\1/'`
@@ -41,13 +44,20 @@ do
         rm ${SCRATCH_FILE}
     ) &
     
+    if [[ $(($CONTEXT % $PARALLEL_LIMIT)) == 0 ]]
+    then
+        # Wait for this whole batch of processes
+        echo "# Waiting for batch"
+        wait
+    fi
+    
 done
 
 # Wait for all the subshells to finish.
 wait
 
 # Aggregate all the output
-cat ${OUT_DIR}/*.tsv
+cat ${OUT_DIR}/*.tsv | sort -n
 
 # Remove aggregated results.
 rm -Rf ${OUT_DIR}
