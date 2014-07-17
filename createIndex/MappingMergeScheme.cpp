a1bbee327e23a6ac48d31be051d29138638ee175
yoheirosen <yrosen@berkeley.edu>
2014-07-17 13:25:35 -0700

    sync to upstream

--- createIndex/MappingMergeScheme.cpp
+++ createIndex/MappingMergeScheme.cpp
@@ -155,6 +155,49 @@ void MappingMergeScheme::generateMerges(size_t queryContig) const {
     // Flip the left mappings back into the original order. They should stay as
     // other-side ranges.
     std::reverse(leftMappings.begin(), leftMappings.end());
+    
+    size_t leftSentinel;
+    size_t rightSentinel;
+    std::vector<size_t> creditCandidates;
+    
+    for(size_t i = 0; i < leftMappings.size(); i++) {
+	// Scan for the first left-mapped position in this contig
+      
+	if(leftMappings[i] != -1) {
+	    if(rightMappings[i] == -1) {
+		leftSentinel = i;
+		break;
+		
+	    } else if(rightMappings[i] != -1 &&
+	      rangeBases[leftMappings[i]].first == rangeBases[rightMappings[i]].first &&
+	      rangeBases[leftMappings[i]].second != rangeBases[rightMappings[i]].second) {
+		leftSentinel = i;
+		break;
+		
+	    }
+	}
+    }
+    
+    for(size_t i = rightMappings.size() - 1; i > -1; i--) {
+	// Scan for the last right-mapped position in this contig
+      
+	if(rightMappings[i] != -1) {
+	    if(leftMappings[i] == -1) {
+		leftSentinel = i;
+		break;
+		
+	    } else if(leftMappings[i] != -1 &&
+	      rangeBases[leftMappings[i]].first == rangeBases[rightMappings[i]].first &&
+	      rangeBases[leftMappings[i]].second != rangeBases[rightMappings[i]].second) {
+		leftSentinel = i;
+		break;
+		
+	    }
+	}
+    }
+    
+    // Now identify and merged mapped bases from the individual left and
+    // right mappings
       
     for(size_t i = 0; i < leftMappings.size(); i++) {
         // For each position, look at the mappings.
@@ -191,7 +234,11 @@ void MappingMergeScheme::generateMerges(size_t queryContig) const {
                 } else {
                     // Didn't map this one
                     unmappedBases++;
+		    if(i > leftSentinel && rightSentinel > i) {
+			creditCandidates.push_back(i);
+		    }
                 }
+                
             } else {
                 // Left mapped and right didn't.
                 
@@ -203,6 +250,7 @@ void MappingMergeScheme::generateMerges(size_t queryContig) const {
                         
                 mappedBases++;
             }
+            
         } else if(rightMappings[i] != -1) {
             // Right mapped and left didn't.
             
@@ -218,8 +266,110 @@ void MappingMergeScheme::generateMerges(size_t queryContig) const {
         } else {
             // Didn't map this one
             unmappedBases++;
-        }
+	    if(i > leftSentinel && rightSentinel > i) {
+		creditCandidates.push_back(i);
+	    
+	    }
+        }   
+    }
+    
+    // Iterate across all possible positions for mapping on credit
+    // TODO: use maximum context lengths to limit search
+    
+    bool firstR;
+    bool contextMappedR;
+    bool firstL;
+    bool contextMappedL;
+    std::pair<std::pair<size_t,size_t>,bool> firstBaseR;
+    std::pair<std::pair<size_t,size_t>,bool> firstBaseL;    
+    
+    size_t maxRContext = 0;
+    size_t maxLContext = 0;
+    
+    for(size_t i; i < rightMappings.size(); i++) {
+	if(rightContext[i] > maxRContext) {
+	    maxRContext = rightContext[i];
+	}
+	if(leftContext[i] > maxLContext) {
+	    maxLContext = leftContext[i];
+	}
+    }
         
+    for(size_t i = 1; i < creditCandidates.size(); i++) {
+	firstR = false;
+	firstL = false;
+	contextMappedR = true;
+	contextMappedL = true;	
+	for(size_t j = creditCandidates[i] - 1; j + 1 > 0 && creditCandidates[i] - j < maxRContext; j--) {
+	    // Search leftward from each position until you find a position
+	    // whose context includes creditCandidate[i]
+	  
+	    if(!firstR) {
+		if(rightContext[j] > creditCandidates[i] - j) {
+		    firstBaseR = rangeBases[rightMappings[j]];
+		    firstR = true;
+		}
+		
+	    // Then continue to search left. If a position has creditCandidate[i]
+	    // in its right context we want to see if subsequent positions map to
+	    // the same place
+	    
+	    // Terminate the search at the first base you come to which no longer
+	    // has our base in its right context. It is not possible that a position
+	    // farther to the left has our base in its right context and continues
+	    // to map to the same place as before
+		
+	    } else {
+		if(rangeBases[rightMappings[j]].first.second != firstBaseR.first.second ||
+			rangeBases[rightMappings[j]].second != firstBaseR.second ||
+			rangeBases[rightMappings[j]].first != firstBaseR.first.first + creditCandidates[i] - j) {
+		    contextMappedR = false;
+		    break;
+		}
+	    }
+	}
+	
+	for(size_t j = creditCandidates[i] + 1; j < creditCandidates.size() && j - creditCandidates[i] < maxLContext; j++) {
+	    if(!firstL) {
+		if(leftContext[j] > j - creditCandidates[i]) {
+		    firstBaseL = rangeBases[rightMappings[j]];
+		    firstL = true;
+		}
+	    } else {
+		if(rangeBases[leftMappings[j]].first.second != firstBaseL.first.second ||
+			rangeBases[leftMappings[j]].second != firstBaseL.second ||
+			rangeBases[leftMappings[j]].first != firstBaseL.first.first + j - creditCandidates[i]) {
+		    contextMappedL = false;
+		    break;
+		}
+	    }
+	}
+		    
+		    
+	if(firstR && contextMappedR) {
+	    firstBaseR.first.first++;
+
+	    if(firstL && contextMappedL) {
+		firstBaseL.first.first--;
+		
+		if(firstBaseR.first = firstBaseL.first &&
+			  firstBaseR.second != firstBaseL.second) {
+		  generateMerge(queryContig, creditCandidates[i], firstBaseR.first.first, 
+                        firstBaseR.first.second, firstBaseR.second);
+		  
+		}
+	    } else {
+		  generateMerge(queryContig, creditCandidates[i], firstBaseR.first.first, 
+                        firstBaseR.first.second, firstBaseR.second);
+	      
+	    }
+	} else if (firstL && contextMappedL) {
+	    firstBaseL.first.first--;
+	    generateMerge(queryContig, creditCandidates[i], firstBaseL.first.first, 
+                        firstBaseL.first.second, !firstBaseL.second);
+	  
+	}
+	
     }
     
     // Close the queue to say we're done.
