4603f87b3425fe90e38187172b1337c53db5ba76
Adam Novak <anovak@soe.ucsc.edu>
2014-08-11 17:19:56 -0700

    Dropping FASTA parser debug stuff

--- speedMap/Fasta.cpp
+++ speedMap/Fasta.cpp
@@ -1,7 +1,5 @@
 #include "Fasta.hpp"
 
-#include <Log.hpp>
-
 #include <cctype>
 
 Fasta::Fasta(std::string filename): stream(filename.c_str()) {
@@ -16,14 +14,10 @@ bool Fasta::hasNext() {
     
     // Look at the next character
     int peeked = stream.peek();
-    Log::info() << "Peeked: " << peeked << "(" << (char)peeked << ")" <<
-        std::endl;
     while(peeked != std::char_traits<char>::eof() && isspace(peeked)) {
         // Scan ahead until it's something important.
         stream.get();
         peeked = stream.peek();
-        Log::info() << "Peeked: " << peeked << "(" << (char)peeked << ")" <<
-            std::endl;
     }
     
     if(!stream.good()) {
@@ -36,7 +30,6 @@ bool Fasta::hasNext() {
         return false;
     }
     
-    Log::info() << "Next exists!" << std::endl;
     
     return true;
 }
@@ -44,13 +37,9 @@ bool Fasta::hasNext() {
 std::string Fasta::getNext() {
     // Keep track of the character we just got
     int got = stream.get();
-    Log::info() << "Got: " << got << "(" << (char)got << ")" <<
-        std::endl;
     while(stream.good() && got != '\n') {
         // Get characters until we hit the end of the first (header) line.
         got = stream.get();
-        Log::info() << "Got: " << got << "(" << (char)got << ")" <<
-            std::endl; 
     }
     
     // We're going to fill up this string.
@@ -58,8 +47,6 @@ std::string Fasta::getNext() {
     
     // Keep track of the next character.
     int peeked = stream.peek();
-    Log::info() << "Peeked: " << peeked << "(" << (char)peeked << ")" <<
-        std::endl;
     while(stream.good() && peeked != '>' && peeked != 
         std::char_traits<char>::eof()) {
         
@@ -75,11 +62,7 @@ std::string Fasta::getNext() {
         
         // Look at the next character.
         peeked = stream.peek();
-        Log::info() << "Peeked: " << peeked << "(" << (char)peeked << ")" <<
-            std::endl;
     }
-    
-    Log::info() << "Found " << toReturn << std::endl;
     
     // Return our built record string.
     return toReturn;
