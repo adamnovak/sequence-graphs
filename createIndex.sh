e6dae70b5fd9eafb9b9dbcf107424b63c906c271
Adam Novak <anovak@soe.ucsc.edu>
2015-05-05 15:18:06 -0700

    Cleaning up a bit

--- createIndex.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Script to run the "createIndex" project
-
-# Get the current directory (see <http://stackoverflow.com/a/246128/402891>)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Run the command. 
-$DIR/createIndex/createIndex "$@"
--- mapToIndex.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Script to run the "mapToIndex" project
-
-# Get the current directory (see <http://stackoverflow.com/a/246128/402891>)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Run the command. Make sure to build with "sbt stage" first.
-$DIR/mapToIndex/target/universal/stage/bin/maptoindex "$@"
