1870e51423fce2f7585ecd0812b94af4db0b69ea
Adam Novak <anovak@soe.ucsc.edu>
2015-05-15 11:58:01 -0700

    Adding a script to run hal2sg on all my Cactus and Camel output. For now I have just hardcoded it to a particular Camel run

---
+++ scripts/continuousDeployment.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# continuousDeployment.sh: Take the Cactus and Camel HAL files, run them through
+# hal2sg, and put them where Maciek can put a server up.
+
+# TODO: make this script re-do the servers, maybe by keying into the AWS commands?
+
+set -e
+
+# What regions do we look at?
+REGIONS="SMA MHC LRC_KIR BRCA1 BRCA2"
+
+function camelFilename()
+{
+    # Return the filename of the Camel HAL for the given upper-case region
+    local REGION=$1
+    
+    # Here is where we pick the scheme to publish
+    echo "output/out3/${REGION}/zipCmis2Min20Ed3Range100.hal"
+}
+
+function cactusFilename()
+{
+    # Return the filename of the Cactus HAL for the given upper-case region
+    local REGION=$1
+    
+    echo "cactus/${REGION,,}_work/${REGION,,}_star.hal"
+}
+
+# Where should we save our hal2sg output?
+OUT_DIR="output/deploy"
+
+for REGION in ${REGIONS}
+do
+    # For each region
+
+    # Where does this region's output go?
+    REGION_OUT_DIR="${OUT_DIR}/${REGION}"
+    
+    # What HAL should Cactus have made?
+    CACTUS_HAL=$(cactusFilename ${REGION})
+    if [ -e ${CACTUS_HAL} ]
+    then
+        # It exists.
+        
+        # Make a cactus directory
+        mkdir -p "${REGION_OUT_DIR}/cactus"
+        
+        # Fill it up with the generated database. TODO: We're supposed to have
+        # --refGenome ref and --noAncestors, but apparently those aren't allowed
+        # for some reason?
+        hal2sg "${CACTUS_HAL}" "${REGION_OUT_DIR}/cactus/database.fa" \
+            "${REGION_OUT_DIR}/cactus/database.sql"
+        
+    fi
+    
+    # What HAL should Camel have made?
+    CAMEL_HAL=$(camelFilename ${REGION})
+    if [ -e ${CAMEL_HAL} ]
+    then
+        # It exists
+        
+        # Make a cactus directory
+        mkdir -p "${REGION_OUT_DIR}/camel"
+        
+        # Fill it up with the generated database
+        hal2sg "${CAMEL_HAL}" "${REGION_OUT_DIR}/camel/database.fa" \
+            "${REGION_OUT_DIR}/camel/database.sql"
+    
+    fi
+
+done
+
+echo "Databases ready for deployment in ${OUT_DIR}"
+
